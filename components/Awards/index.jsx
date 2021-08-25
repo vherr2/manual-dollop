c5250670d34f42e7060f644b3257bcb00aa96f79
Vincent Herr <vherr2@illinois.edu>
2021-08-24 22:22:49 -0700

    feat(org_drilldown): adds navigation from awards table to filer org drilldown

--- components/Awards/index.jsx
+++ components/Awards/index.jsx
@@ -1,6 +1,11 @@
+import { useRouter } from 'next/router'
 import { Table } from 'components/Table'
 
 const col = [
+  {
+    name: 'Filer Name',
+    field: 'filer_name',
+  },
   {
     name: 'Amount',
     field: 'amount'
@@ -11,17 +16,27 @@ const col = [
   }
 ];
 
-const currencyFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+const currencyFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
 
 export const Awards = (props) => {
   const awards =
     props.data
-    .map((award) => new Map(Object.entries(award)))
-    .map((award) => award.set('amount', currencyFormatter.format(award.get('amount'))));
+      .map((award) => new Map(Object.entries(award)))
+      .map((award) => award.set('amount', currencyFormatter.format(award.get('amount'))));
+
+  const router = useRouter();
+
+  const handleClick = (id) => {
+    router.push({
+      pathname: '/organizations/[id]',
+      query: { id: id }
+    });
+  }
 
   const body = awards.map((award, idx) => {
     return (
-      <tr key={idx}>
+      <tr key={idx} onClick={() => handleClick(award.get('filer_id'))}>
+        <td> {award.get('filer_name')} </td>
         <td> {award.get('amount')} </td>
         <td> {award.get('purpose')} </td>
       </tr>
